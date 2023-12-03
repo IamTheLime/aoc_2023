5350eb302f7e23f39ab8b4a509fed8ff5999a137
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-12-03 20:31:18 +0000

    reformat

--- aoc_ts/src/aoc.ts
+++ aoc_ts/src/aoc.ts
@@ -4,169 +4,169 @@ import { parse } from "url";
 type StringVectorOutput = String[];
 
 enum OutputStyle {
-  StringVector,
+    StringVector,
 }
 
 function read_input(
-  input_location: PathLike,
-  expected_output: OutputStyle,
+    input_location: PathLike,
+    expected_output: OutputStyle,
 ): StringVectorOutput {
-  const file_buffer = readFileSync(input_location, {
-    encoding: "utf8",
-    flag: "r",
-  });
+    const file_buffer = readFileSync(input_location, {
+        encoding: "utf8",
+        flag: "r",
+    });
 
-  switch (expected_output) {
-    case OutputStyle.StringVector:
-      return file_buffer.split("\n").filter((x) => x !== "");
-    default:
-      throw "What the fuck is happening here";
-  }
+    switch (expected_output) {
+        case OutputStyle.StringVector:
+            return file_buffer.split("\n").filter((x) => x !== "");
+        default:
+            throw "What the fuck is happening here";
+    }
 }
 
 type Optional<T> = null | T;
 
 function check_if_number(array_slice: string[]): Optional<string> {
-  if (array_slice[0] === undefined) return null;
-  const numbers: Record<string, string> = {
-    one: "1",
-    two: "2",
-    three: "3",
-    four: "4",
-    five: "5",
-    six: "6",
-    seven: "7",
-    eight: "8",
-    nine: "9",
-  };
-  const int_conversion_attempt = parseInt(array_slice[0]);
-  if (int_conversion_attempt) {
-    return array_slice[0];
-  }
-  for (const entry of Object.keys(numbers)) {
-    const potential_number = array_slice.slice(0, entry.length);
-
-    if (entry === potential_number.join("")) {
-      return <string>numbers[entry];
+    if (array_slice[0] === undefined) return null;
+    const numbers: Record<string, string> = {
+        one: "1",
+        two: "2",
+        three: "3",
+        four: "4",
+        five: "5",
+        six: "6",
+        seven: "7",
+        eight: "8",
+        nine: "9",
+    };
+    const int_conversion_attempt = parseInt(array_slice[0]);
+    if (int_conversion_attempt) {
+        return array_slice[0];
     }
-  }
+    for (const entry of Object.keys(numbers)) {
+        const potential_number = array_slice.slice(0, entry.length);
 
-  return null;
+        if (entry === potential_number.join("")) {
+            return <string>numbers[entry];
+        }
+    }
+
+    return null;
 }
 
 function one(): void {
-  const readFile = read_input("./inputs/1.input", OutputStyle.StringVector);
+    const readFile = read_input("./inputs/1.input", OutputStyle.StringVector);
 
-  let res = readFile.map((line) => {
-    return line
-      .split("")
-      .map((_: string, index: number, array: string[]) => {
-        let res = check_if_number(array.slice(index));
-        if (res) {
-          return res;
-        }
-        return undefined;
-      })
-      .filter((x) => x !== undefined);
-  });
+    let res = readFile.map((line) => {
+        return line
+            .split("")
+            .map((_: string, index: number, array: string[]) => {
+                let res = check_if_number(array.slice(index));
+                if (res) {
+                    return res;
+                }
+                return undefined;
+            })
+            .filter((x) => x !== undefined);
+    });
 
-  let final_value = res
-    .map((line) => parseInt(`${line[0]}${line[line.length - 1]}`))
-    .reduce((x, y) => x + y);
+    let final_value = res
+        .map((line) => parseInt(`${line[0]}${line[line.length - 1]}`))
+        .reduce((x, y) => x + y);
 
-  console.log(final_value);
+    console.log(final_value);
 }
 ////////////////////////////////////////////////////////////////////////////////
 
 type RGB = Record<"red" | "green" | "blue", number>;
 
 function two(): void {
-  const readFile = read_input("./inputs/2.input", OutputStyle.StringVector);
+    const readFile = read_input("./inputs/2.input", OutputStyle.StringVector);
 
-  const bagRgb: RGB = {
-    red: 12,
-    green: 13,
-    blue: 14,
-  };
+    const bagRgb: RGB = {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
 
-  const games = readFile.map((game) => {
-    const info = <RGB[]>game
-      .split(":")[1]
-      ?.split(";")
-      .map((draw) => {
-        let removedRgb: RGB = {
-          red: 0,
-          green: 0,
-          blue: 0,
-        };
-        draw.split(",").map((colour) => {
-          const colourCount = colour.trim().split(" ");
-          removedRgb[<"red" | "green" | "blue">colourCount[1]] = <number>(
-            parseInt(<string>colourCount[0])
-          );
-        });
-        return removedRgb;
-      });
-    return info;
-  });
+    const games = readFile.map((game) => {
+        const info = <RGB[]>game
+            .split(":")[1]
+            ?.split(";")
+            .map((draw) => {
+                let removedRgb: RGB = {
+                    red: 0,
+                    green: 0,
+                    blue: 0,
+                };
+                draw.split(",").map((colour) => {
+                    const colourCount = colour.trim().split(" ");
+                    removedRgb[<"red" | "green" | "blue">colourCount[1]] = <number>(
+                        parseInt(<string>colourCount[0])
+                    );
+                });
+                return removedRgb;
+            });
+        return info;
+    });
 
-  let playableGames = [];
+    let playableGames = [];
 
-  const res_part_1 = games
-    .map((game, index) => {
-      return game.reduce((acc, current) => {
-        let attempt_current = true;
-        for (const rgb of Object.keys(bagRgb)) {
-          if (
-            bagRgb[<"red" | "green" | "blue">rgb] <
-            current[<"red" | "green" | "blue">rgb]
-          ) {
-            attempt_current = false;
-            break;
-          }
-        }
-        return acc && attempt_current;
-      }, true);
-    })
-    .reduce((acc, current, idx) => acc + (current ? idx + 1 : 0), 0);
-  console.log("part 1", res_part_1);
+    const res_part_1 = games
+        .map((game, index) => {
+            return game.reduce((acc, current) => {
+                let attempt_current = true;
+                for (const rgb of Object.keys(bagRgb)) {
+                    if (
+                        bagRgb[<"red" | "green" | "blue">rgb] <
+                        current[<"red" | "green" | "blue">rgb]
+                    ) {
+                        attempt_current = false;
+                        break;
+                    }
+                }
+                return acc && attempt_current;
+            }, true);
+        })
+        .reduce((acc, current, idx) => acc + (current ? idx + 1 : 0), 0);
+    console.log("part 1", res_part_1);
 
-  let res_part_2 = games
-    .map((game) => {
-      return game.reduce(
-        (acc, current) => {
-          acc.red = acc.red > current.red ? acc.red : current.red;
-          acc.blue = acc.blue > current.blue ? acc.blue : current.blue;
-          acc.green = acc.green > current.green ? acc.green : current.green;
-          return acc;
-        },
-        { red: 0, green: 0, blue: 0 },
-      );
-    })
-    .reduce((acc, game) => {
-      return (
-        acc +
-        Object.values(game).reduce((acc, colourCount) => acc * colourCount, 1)
-      );
-    }, 0);
-  console.log(res_part_2);
+    let res_part_2 = games
+        .map((game) => {
+            return game.reduce(
+                (acc, current) => {
+                    acc.red = acc.red > current.red ? acc.red : current.red;
+                    acc.blue = acc.blue > current.blue ? acc.blue : current.blue;
+                    acc.green = acc.green > current.green ? acc.green : current.green;
+                    return acc;
+                },
+                { red: 0, green: 0, blue: 0 },
+            );
+        })
+        .reduce((acc, game) => {
+            return (
+                acc +
+                Object.values(game).reduce((acc, colourCount) => acc * colourCount, 1)
+            );
+        }, 0);
+    console.log(res_part_2);
 }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 type CoordinatesInfoSymbol = {
-    x: number 
-    y: number 
+    x: number
+    y: number
     symbol: "empty" | "symbol" | "gear"
-    gear_neighbours: number[] 
+    gear_neighbours: number[]
 }
 
 
 type CoordinatesInfoNumber = {
     x: number
-    y: number 
-    uinique_identifier: string 
+    y: number
+    uinique_identifier: string
     symbol: "number"
     value: number
     has_adjacent: boolean
@@ -174,71 +174,71 @@ type CoordinatesInfoNumber = {
 
 type CoordinatesInfo = CoordinatesInfoNumber | CoordinatesInfoSymbol
 
-function range(start:number, end:number, step:number = 1): number[] {
-  let output = [];
-  if (typeof end === 'undefined') {
-    end = start;
-    start = 0;
-  }
-  for (let i = start; i < end; i += step) {
-    output.push(i);
-  }
-  return output;
+function range(start: number, end: number, step: number = 1): number[] {
+    let output = [];
+    if (typeof end === 'undefined') {
+        end = start;
+        start = 0;
+    }
+    for (let i = start; i < end; i += step) {
+        output.push(i);
+    }
+    return output;
 }
 
 function three(): void {
-  const readFile = read_input("./inputs/3.input", OutputStyle.StringVector);
+    const readFile = read_input("./inputs/3.input", OutputStyle.StringVector);
     const parsed_file = readFile.map((row, y_idx) => {
         let new_row = row.split(/((?=[^\d])|(?<=[^\d]))/).filter(x => x !== "");
         let track_x_shift = 0;
-        let parsed_row = new_row.map( (entry, index)  => {
+        let parsed_row = new_row.map((entry, index) => {
             let min_x = index + track_x_shift;
             let max_x = (entry.length > 1) ? min_x + entry.length - 1 : min_x;
-            track_x_shift += (entry.length > 1) ? entry.length-1 : 0
+            track_x_shift += (entry.length > 1) ? entry.length - 1 : 0
             switch (entry) {
                 case entry.match(/[^\d]/)?.input:
                     return <CoordinatesInfoSymbol>{
                         x: min_x,
                         y: y_idx,
-                        symbol: entry === "." ? "empty" : (entry === "*" ? "gear" : "symbol"), 
+                        symbol: entry === "." ? "empty" : (entry === "*" ? "gear" : "symbol"),
                         gear_neighbours: []
                     }
                 default:
-                    return range(min_x,max_x+1).map( (x) => {
+                    return range(min_x, max_x + 1).map((x) => {
                         return <CoordinatesInfoNumber>{
                             uinique_identifier: `${y_idx}${min_x}${max_x}`,
-                            x:x,
+                            x: x,
                             y: y_idx,
                             symbol: "number",
                             value: parseInt(entry, 10),
-                            has_adjacent: false 
+                            has_adjacent: false
                         }
                     })
             }
-        }) 
+        })
         return parsed_row.flat();
     });
-   
+
     parsed_file.map((row, y) => {
         row.map((col, x) => {
-            if (col.symbol === "symbol" || col.symbol === "gear"){
+            if (col.symbol === "symbol" || col.symbol === "gear") {
                 // console.log(col)
-               range(x-1,x+2).map((x) => range(y-1, y+2).map((y)=>{
+                range(x - 1, x + 2).map((x) => range(y - 1, y + 2).map((y) => {
                     const entry = parsed_file[y]?.[x];
                     if (entry?.symbol === "number") {
-                       entry.has_adjacent = true; 
+                        entry.has_adjacent = true;
                     }
-                })) 
+                }))
             }
-        
-            if (col.symbol === "gear"){
+
+            if (col.symbol === "gear") {
 
                 let gear_neighbours: Record<string, number> = {}
                 // console.log(col)
-               range(x-1,x+2).map((x) => range(y-1, y+2).map((y)=>{
+                range(x - 1, x + 2).map((x) => range(y - 1, y + 2).map((y) => {
                     const entry = parsed_file[y]?.[x];
                     if (entry?.symbol === "number") {
-                      gear_neighbours[entry.uinique_identifier] = entry.value; 
+                        gear_neighbours[entry.uinique_identifier] = entry.value;
                     }
                 }));
 
@@ -247,9 +247,9 @@ function three(): void {
 
         })
     })
-    
+
     let found_numbers: Record<string, number> = {}
-    
+
     for (let entry of parsed_file.flat()) {
         if (entry.symbol === "number" && entry.has_adjacent === true) {
             found_numbers[entry.uinique_identifier] = entry.value;
@@ -258,19 +258,19 @@ function three(): void {
 
     console.log(found_numbers)
     console.log(Object.values(found_numbers).reduce((acc, value) => acc + value));
-    
+
     const gears_res = parsed_file.flat().filter(
         (val) => val.symbol === "gear" && val.gear_neighbours.length == 2
     ).map((x) => {
         return (<CoordinatesInfoSymbol>x).gear_neighbours.reduce((acc, val) => acc * val);
-    }).reduce((acc,x) => acc + x)
+    }).reduce((acc, x) => acc + x)
     console.log(gears_res)
 
 }
 
 function main(): void {
-  // one();
-  // two();
+    // one();
+    // two();
     three();
 }
 
